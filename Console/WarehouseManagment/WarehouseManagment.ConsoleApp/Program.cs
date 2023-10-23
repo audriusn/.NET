efd2b03ff6d54d77fa0672c66087258f08938e0d
audriusn <audrius.narkunas@gmail.com>
2023-10-23 20:44:08 +0300

    Initial console example

---
+++ Console/WarehouseManagment/WarehouseManagment.ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using WarehouseManagment.ConsoleApp.Services;
+
+namespace WarehouseManagment.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var applicationService = new ApplicationService();
+
+                Console.WriteLine("Enter your command:");
+                var command = Console.ReadLine();
+
+                applicationService.Process(command);
+            }      
+        }
+    }
+}
---
+++ Console/WarehouseManagment/WarehouseManagment.ConsoleApp/Services/ApplicationService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseManagment.ConsoleApp.Services
+{
+    internal class ApplicationService
+    {
+        private WarehouseService _warehouseService;
+
+        public ApplicationService()
+        {
+            _warehouseService = new WarehouseService();
+        }
+        public void Process (string command)
+        {
+            if (command.StartsWith("Add"))
+            {
+                _warehouseService.Add();
+            }
+            if (command.StartsWith("Remove"))
+            {
+                _warehouseService.Remove();
+            }
+            if (command.StartsWith("List"))
+            {
+                _warehouseService.List();
+            }
+            if (command.StartsWith("Exit"))
+            {
+                return;
+            }
+            Console.WriteLine("Incorrect command");
+         
+        }
+    }
+}
