225d2438aa5e3d89a4458791556cbd3b7ff52151
audriusn <audrius.narkunas@gmail.com>
2024-05-19 17:34:46 +0300

    Home task

---
+++ HomeTask_UVS/HomeTask_UVS/Models/Thread.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTask_UVS.Models
+{
+    internal class Thread
+    {
+        public int ThreadID { get; set; }
+        public string GeneratedString { get; set; }
+    }
+}
+
